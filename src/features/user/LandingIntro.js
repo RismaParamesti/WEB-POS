0ed07c3f702892d5d9cf362e68c64492a4305964
nandakharismas22 <126435181+nandakharismas22@users.noreply.github.com>
2024-11-28 15:01:29 +0700

    landing dan login

--- src/features/user/LandingIntro.js
+++ src/features/user/LandingIntro.js
@@ -4,23 +4,32 @@ import TemplatePointers from "./components/TemplatePointers"
 
 function LandingIntro(){
 
-    return(
-        <div className="hero min-h-full rounded-l-xl bg-base-200">
-            <div className="hero-content py-12">
-              <div className="max-w-md">
+  return (
+    // <div className="hero min-h-full rounded-l-xl bg-base-200 relative">
+    <div className="hero min-h-full rounded-l-xl relative">
+            <img src="./LandBg.svg" alt="Background" className="absolute top-0 left-0 w-full h-full object-cover opacity-90 z-0" />
+    {/* Gambar TransLogi di atas background */}
+      <div className="hero-content py-12">
+        <div className="max-w-md">
+          {/* Logo dan title */}
+          <div className="text-center">
+            <img 
+              src="LogoIconLogin.svg" 
+              className="w-30 inline-block mask mask-circle mb-2" 
+              alt="dashwind-logo" 
+            />
+            {/* <h1 className="text-2xl font-bold">DashStack</h1> */}
+            <h1 className="text-2xl mt-0 font-bold text-center">
+    <span className="text-primary">Dash</span>
+    <span className="text-info-content">Stack</span>
+</h1>
 
-              <h1 className='text-3xl text-center font-bold '><img src="/logo192.png" className="w-12 inline-block mr-2 mask mask-circle" alt="dashwind-logo" />DashWind</h1>
-
-                <div className="text-center mt-12"><img src="./intro.png" alt="Dashwind Admin Template" className="w-48 inline-block"></img></div>
-              
-              {/* Importing pointers component */}
-              <TemplatePointers />
-              
-              </div>
-
-            </div>
           </div>
-    )
+          <TemplatePointers />
+        </div>
+      </div>
+    </div>
+  );
       
   }
   
