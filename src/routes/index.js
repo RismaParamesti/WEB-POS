e0a1ee481ecc94b427ab59a9a1d3c1e3349b4630
rrsfira31 <rrachmashafira@gmail.com>
2024-12-09 20:28:27 +0700

    revisi report

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,135 +1,202 @@
 // All components mapping with path for internal routes
 
-import { lazy } from 'react'
+import { lazy } from "react";
+import CreateProduct from "../features/product/createProduct";
+import AllProducts from "../features/product/allProduct";
+import CountStock from "../features/product/countStock";
+import CategoryPage from "../features/product/category";
+import PrintLabels from "../features/product/printLabels";
+import UnitPage from "../features/product/unit";
+import BrandPage from "../features/product/brand";
+import DetailProduct from "../features/product/allProduct/detailProduct";
+import EditProduct from "../features/product/allProduct/editProduct";
+import Warehouse from "../features/warehouse/warehouse";
+import CreateTransfer from "../features/warehouse/createTransfer";
+import AllTransfer from "../features/warehouse/allTransfer";
+import DetailTransfer from "../features/warehouse/detailTransfer";
+import EditTransfer from "../features/warehouse/editTransfer";
 
-const Dashboard = lazy(() => import('../pages/protected/Dashboard'))
-const Welcome = lazy(() => import('../pages/protected/Welcome'))
-const Page404 = lazy(() => import('../pages/protected/404'))
-const Blank = lazy(() => import('../pages/protected/Blank'))
-const Charts = lazy(() => import('../pages/protected/Charts'))
-const Leads = lazy(() => import('../pages/protected/Leads'))
-const Integration = lazy(() => import('../pages/protected/Integration'))
-const Calendar = lazy(() => import('../pages/protected/Calendar'))
-const Team = lazy(() => import('../pages/protected/Team'))
-const Transactions = lazy(() => import('../pages/protected/Transactions'))
-const Bills = lazy(() => import('../pages/protected/Bills'))
-const ProfileSettings = lazy(() => import('../pages/protected/ProfileSettings'))
-const GettingStarted = lazy(() => import('../pages/GettingStarted'))
-const DocFeatures = lazy(() => import('../pages/DocFeatures'))
-const DocComponents = lazy(() => import('../pages/DocComponents'))
-const SalesReport = lazy(() => import('../pages/protected/SalesReport'))
-const ProductReport = lazy(() => import('../pages/protected/ProductReport'))
-const PSalesReport = lazy(() => import('../pages/protected/PSalesReport'))
-const PurchaseReport = lazy(() => import('../pages/protected/PurchaseReport'))
-const StockReport = lazy(() => import('../pages/protected/StockReport'))
-const SupplierReport = lazy(() => import('../pages/protected/SupplierReport'))
-const QtyAlert = lazy(() => import('../pages/protected/QtyAlert'))
-const CustomerReport = lazy(() => import('../pages/protected/CustomerReport'))
-const BestCustomer = lazy(() => import('../pages/protected/BestCustomer'))
-const TopSell = lazy(() => import('../pages/protected/TopSell'))
-
+const Dashboard = lazy(() => import("../pages/protected/Dashboard"));
+const Welcome = lazy(() => import("../pages/protected/Welcome"));
+const Page404 = lazy(() => import("../pages/protected/404"));
+const Blank = lazy(() => import("../pages/protected/Blank"));
+const Charts = lazy(() => import("../pages/protected/Charts"));
+const Leads = lazy(() => import("../pages/protected/Leads"));
+const Integration = lazy(() => import("../pages/protected/Integration"));
+const Calendar = lazy(() => import("../pages/protected/Calendar"));
+const Team = lazy(() => import("../pages/protected/Team"));
+const Transactions = lazy(() => import("../pages/protected/Transactions"));
+const Bills = lazy(() => import("../pages/protected/Bills"));
+const ProfileSettings = lazy(() =>
+  import("../pages/protected/ProfileSettings")
+);
+const GettingStarted = lazy(() => import("../pages/GettingStarted"));
+const DocFeatures = lazy(() => import("../pages/DocFeatures"));
+const DocComponents = lazy(() => import("../pages/DocComponents"));
+const SalesReport = lazy(() => import("../pages/protected/SalesReport"));
+const ProductReport = lazy(() => import("../pages/protected/ProductReport"));
+const PSalesReport = lazy(() => import("../pages/protected/PSalesReport"));
+const PurchaseReport = lazy(() => import("../pages/protected/PurchaseReport"));
+const StockReport = lazy(() => import("../pages/protected/StockReport"));
+const SupplierReport = lazy(() => import("../pages/protected/SupplierReport"));
+const QtyAlert = lazy(() => import("../pages/protected/QtyAlert"));
+const CustomerReport = lazy(() => import("../pages/protected/CustomerReport"));
+const BestCustomer = lazy(() => import("../pages/protected/BestCustomer"));
+const TopSell = lazy(() => import("../pages/protected/TopSell"));
 
 const routes = [
   {
-    path: '/dashboard', // the url
+    path: "/dashboard", // the url
     component: Dashboard, // view rendered
   },
   {
-    path: '/welcome', // the url
+    path: "/welcome", // the url
     component: Welcome, // view rendered
   },
   {
-    path: '/leads',
-    component: Leads,
+    path: "/product-createProduct",
+    component: CreateProduct,
   },
   {
-    path: '/settings-team',
+    path: "/product-allProduct",
+    component: AllProducts,
+  },
+  {
+    path: "/product-allProduct/detailProduct",
+    component: DetailProduct,
+  },
+  {
+    path: "/product-allProduct/editProduct",
+    component: EditProduct,
+  },
+  {
+    path: "/product-countStock",
+    component: CountStock,
+  },
+  {
+    path: "/product-category",
+    component: CategoryPage,
+  },
+  {
+    path: "/product-printLabels",
+    component: PrintLabels,
+  },
+  {
+    path: "/product-unit",
+    component: UnitPage,
+  },
+  {
+    path: "/product-brand",
+    component: BrandPage,
+  },
+  {
+    path: "/warehouse-warehouse",
+    component: Warehouse,
+  },
+  {
+    path: "/warehouse-createTransfer",
+    component: CreateTransfer,
+  },
+  {
+    path: "/warehouse-allTransfer",
+    component: AllTransfer,
+  },
+  {
+    path: "/warehouse-detailTransfer",
+    component: DetailTransfer,
+  },
+  {
+    path: "/warehouse-editTransfer",
+    component: EditTransfer,
+  },
+  {
+    path: "/settings-team",
     component: Team,
   },
   {
-    path: '/calendar',
+    path: "/calendar",
     component: Calendar,
   },
   {
-    path: '/transactions',
+    path: "/transactions",
     component: Transactions,
   },
   {
-    path: '/settings-profile',
+    path: "/settings-profile",
     component: ProfileSettings,
   },
   {
-    path: '/settings-billing',
+    path: "/settings-billing",
     component: Bills,
   },
   {
-    path: '/getting-started',
+    path: "/getting-started",
     component: GettingStarted,
   },
   {
-    path: '/features',
+    path: "/features",
     component: DocFeatures,
   },
   {
-    path: '/components',
+    path: "/components",
     component: DocComponents,
   },
   {
-    path: '/integration',
+    path: "/integration",
     component: Integration,
   },
   {
-    path: '/charts',
+    path: "/charts",
     component: Charts,
   },
   {
-    path: '/404',
+    path: "/404",
     component: Page404,
   },
   {
-    path: '/blank',
+    path: "/blank",
     component: Blank,
   },
   {
-    path: '/SalesReport',
+    path: "/SalesReport",
     component: SalesReport,
   },
   {
-    path: '/ProductReport',
+    path: "/ProductReport",
     component: ProductReport,
   },
   {
-    path: '/PSalesReport',
+    path: "/PSalesReport",
     component: PSalesReport,
   },
   {
-    path: '/PurchaseReport',
+    path: "/PurchaseReport",
     component: PurchaseReport,
   },
   {
-    path: '/StockReport',
+    path: "/StockReport",
     component: StockReport,
   },
   {
-    path: '/SupplierReport',
+    path: "/SupplierReport",
     component: SupplierReport,
   },
   {
-    path: '/QtyAlert',
+    path: "/QtyAlert",
     component: QtyAlert,
   },
   {
-    path: '/CustomerReport',
+    path: "/CustomerReport",
     component: CustomerReport,
   },
   {
-    path: '/BestCustomer',
+    path: "/BestCustomer",
     component: BestCustomer,
   },
   {
-    path: '/TopSell',
+    path: "/TopSell",
     component: TopSell,
   },
-]
+];
 
-export default routes
+export default routes;
--- src/routes/sidebar.js
+++ src/routes/sidebar.js
@@ -1,23 +1,23 @@
 /** Icons are imported separatly to reduce build time */
-import BellIcon from '@heroicons/react/24/outline/BellIcon'
-import DocumentTextIcon from '@heroicons/react/24/outline/DocumentTextIcon'
-import Squares2X2Icon from '@heroicons/react/24/outline/Squares2X2Icon'
-import TableCellsIcon from '@heroicons/react/24/outline/TableCellsIcon'
-import WalletIcon from '@heroicons/react/24/outline/WalletIcon'
-import CodeBracketSquareIcon from '@heroicons/react/24/outline/CodeBracketSquareIcon'
-import DocumentIcon from '@heroicons/react/24/outline/DocumentIcon'
-import ExclamationTriangleIcon from '@heroicons/react/24/outline/ExclamationTriangleIcon'
-import CalendarDaysIcon from '@heroicons/react/24/outline/CalendarDaysIcon'
-import ArrowRightOnRectangleIcon from '@heroicons/react/24/outline/ArrowRightOnRectangleIcon'
-import UserIcon from '@heroicons/react/24/outline/UserIcon'
-import Cog6ToothIcon from '@heroicons/react/24/outline/Cog6ToothIcon'
-import BoltIcon from '@heroicons/react/24/outline/BoltIcon'
-import ChartBarIcon from '@heroicons/react/24/outline/ChartBarIcon'
-import CurrencyDollarIcon from '@heroicons/react/24/outline/CurrencyDollarIcon'
-import InboxArrowDownIcon from '@heroicons/react/24/outline/InboxArrowDownIcon'
-import UsersIcon from '@heroicons/react/24/outline/UsersIcon'
-import KeyIcon from '@heroicons/react/24/outline/KeyIcon'
-import DocumentDuplicateIcon from '@heroicons/react/24/outline/DocumentDuplicateIcon'
+import BellIcon from "@heroicons/react/24/outline/BellIcon";
+import DocumentTextIcon from "@heroicons/react/24/outline/DocumentTextIcon";
+import Squares2X2Icon from "@heroicons/react/24/outline/Squares2X2Icon";
+import TableCellsIcon from "@heroicons/react/24/outline/TableCellsIcon";
+import WalletIcon from "@heroicons/react/24/outline/WalletIcon";
+import CodeBracketSquareIcon from "@heroicons/react/24/outline/CodeBracketSquareIcon";
+import DocumentIcon from "@heroicons/react/24/outline/DocumentIcon";
+import ExclamationTriangleIcon from "@heroicons/react/24/outline/ExclamationTriangleIcon";
+import CalendarDaysIcon from "@heroicons/react/24/outline/CalendarDaysIcon";
+import ArrowRightOnRectangleIcon from "@heroicons/react/24/outline/ArrowRightOnRectangleIcon";
+import UserIcon from "@heroicons/react/24/outline/UserIcon";
+import Cog6ToothIcon from "@heroicons/react/24/outline/Cog6ToothIcon";
+import BoltIcon from "@heroicons/react/24/outline/BoltIcon";
+import ChartBarIcon from "@heroicons/react/24/outline/ChartBarIcon";
+import CurrencyDollarIcon from "@heroicons/react/24/outline/CurrencyDollarIcon";
+import InboxArrowDownIcon from "@heroicons/react/24/outline/InboxArrowDownIcon";
+import UsersIcon from "@heroicons/react/24/outline/UsersIcon";
+import KeyIcon from "@heroicons/react/24/outline/KeyIcon";
+import DocumentDuplicateIcon from "@heroicons/react/24/outline/DocumentDuplicateIcon";
 import {
   ChevronDownIcon,
   FolderIcon,
@@ -28,190 +28,182 @@ import {
   SwatchIcon,
 } from "@heroicons/react/24/outline";
 
-const iconClasses = `h-6 w-6`
-const submenuIconClasses = `h-5 w-5`
+const iconClasses = `h-6 w-6`;
+const submenuIconClasses = `h-5 w-5`;
 
 const routes = [
-
   {
-    path: '/app/dashboard',
-    icon: <Squares2X2Icon className={iconClasses}/>, 
-    name: 'Dashboard',
+    path: "/app/dashboard",
+    icon: <Squares2X2Icon className={iconClasses} />,
+    name: "Dashboard",
   },
 
   {
-    path: '', //no url needed as this has submenu
-    icon: <FolderIcon className={`${iconClasses} inline` }/>, // icon component
-    name: 'Product', // name that appear in Sidebar
-    submenu : [
+    path: "", //no url needed as this has submenu
+    icon: <FolderIcon className={`${iconClasses} inline`} />, // icon component
+    name: "Product", // name that appear in Sidebar
+    submenu: [
       {
-        path: '/app/CreateProduct',
-        name: 'Create Product',
+        path: "/app/product-createProduct",
+        name: "Create Product",
       },
       {
-        path: '/app/AllProducts', //url
-        name: 'All Products', // name that appear in Sidebar
+        path: "/app/product-allProduct", //url
+        name: "All Products", // name that appear in Sidebar
       },
       {
-        path: '/app/CountStock',
-        name: 'Count Stock',
+        path: "/app/product-countStock",
+        name: "Count Stock",
       },
       {
-        path: '/app/Category',
-        name: 'Category',
+        path: "/app/product-category",
+        name: "Category",
       },
       {
-        path: '/app/PrintLabels',
-        name: 'Print Labels',
+        path: "/app/product-printLabels",
+        name: "Print Labels",
       },
-      // {
-      //   path: '/app/blank',
-      //   name: 'Unit',
-      // },
-      // {
-      //   path: '/app/blank',
-      //   name: 'Brand',
-      // },
-      
-    ]
+      {
+        path: "/app/product-unit",
+        name: "Unit",
+      },
+      {
+        path: "/app/product-brand",
+        name: "Brand",
+      },
+    ],
   },
   {
-    path: '', //no url needed as this has submenu
-    icon: <InboxStackIcon className={`${iconClasses} inline` } />, // icon component
-    name: 'Inventory', // name that appear in Sidebar
-    submenu : [
+    path: "", //no url needed as this has submenu
+    icon: <InboxStackIcon className={`${iconClasses} inline`} />, // icon component
+    name: "Inventory", // name that appear in Sidebar
+    submenu: [
       {
-        path: '/app/CreatePurchase',
-        name: 'Create Purchase',
+        path: "/app/CreatePurchase",
+        name: "Create Purchase",
       },
       {
-        path: '/app/AllPurchases',
-        name: 'All Purchases',
+        path: "/app/AllPurchases",
+        name: "All Purchases",
       },
       {
-        path: '/app/ReturnPurchases',
-        name: 'Return Purchases',
+        path: "/app/ReturnPurchases",
+        name: "Return Purchases",
       },
-      
-    ]
+    ],
   },
 
   {
-    path: '', //no url needed as this has submenu
-    icon: <IdentificationIcon className={`${iconClasses} inline` } />, // icon component
-    name: 'People', // name that appear in Sidebar
-    submenu : [
+    path: "", //no url needed as this has submenu
+    icon: <IdentificationIcon className={`${iconClasses} inline`} />, // icon component
+    name: "People", // name that appear in Sidebar
+    submenu: [
       {
-        path: '/app/Customers',
-        name: 'Customers',
+        path: "/app/Customers",
+        name: "Customers",
       },
       {
-        path: '/app/Suppliers',
-        name: 'Suppliers',
+        path: "/app/Suppliers",
+        name: "Suppliers",
       },
       {
-        path: '/app/User',
-        name: 'User',
+        path: "/app/User",
+        name: "User",
       },
-      
-    ]
+    ],
   },
 
   {
-    path: '', //no url needed as this has submenu
-    icon: <SwatchIcon className={`${iconClasses} inline` } />, // icon component
-    name: 'Sales', // name that appear in Sidebar
-    submenu : [
+    path: "", //no url needed as this has submenu
+    icon: <SwatchIcon className={`${iconClasses} inline`} />, // icon component
+    name: "Sales", // name that appear in Sidebar
+    submenu: [
       {
-        path: '/app/CreateSale',
-        name: 'Create Sale',
+        path: "/app/CreateSale",
+        name: "Create Sale",
       },
       {
-        path: '/app/AllSales',
-        name: 'All Sales',
+        path: "/app/AllSales",
+        name: "All Sales",
       },
       // {
       //   path: '/app/Pos',
       //   name: 'POS',
       // },
       {
-        path: '/app/ReturnSales',
-        name: 'Return Sales',
+        path: "/app/ReturnSales",
+        name: "Return Sales",
       },
-    ]
+    ],
   },
 
   {
-    path: '', //no url needed as this has submenu
-    icon: <HomeModernIcon className={`${iconClasses} inline` } />, // icon component
-    name: 'Warehouse', // name that appear in Sidebar
-    submenu : [
+    path: "", //no url needed as this has submenu
+    icon: <HomeModernIcon className={`${iconClasses} inline`} />, // icon component
+    name: "Warehouse", // name that appear in Sidebar
+    submenu: [
       {
-        path: '/app/Warehouses',
-        name: 'Warehouses',
+        path: "/app/warehouse-warehouse",
+        name: "Warehouses",
       },
       {
-        path: '/app/CreateTransfer',
-        name: 'Create Transfer',
+        path: "/app/warehouse-createTransfer",
+        name: "Create Transfer",
       },
       {
-        path: '/app/AllTransfer',
-        name: 'All Transfer',
+        path: "/app/warehouse-allTransfer",
+        name: "All Transfer",
       },
-    ]
+    ],
   },
 
   {
-    path: '', //no url needed as this has submenu
-    icon: <ChartBarIcon className={`${iconClasses} inline` } />, // icon component
-    name: 'Report', // name that appear in Sidebar
-    submenu : [
+    path: "", //no url needed as this has submenu
+    icon: <ChartBarIcon className={`${iconClasses} inline`} />, // icon component
+    name: "Report", // name that appear in Sidebar
+    submenu: [
       {
-        path: '/app/SalesReport',
-        name: 'Sales Report',
+        path: "/app/SalesReport",
+        name: "Sales Report",
       },
       {
-        path: '/app/ProductReport',
-        name: 'Product Report',
+        path: "/app/ProductReport",
+        name: "Product Report",
       },
       {
-        path: '/app/PSalesReport',
-        name: 'Product Sales Report',
+        path: "/app/PSalesReport",
+        name: "Product Sales Report",
       },
       {
-        path: '/app/PurchaseReport',
-        name: 'Purchase Report',
+        path: "/app/PurchaseReport",
+        name: "Purchase Report",
       },
       {
-        path: '/app/StockReport',
-        name: 'Stock Report',
+        path: "/app/StockReport",
+        name: "Stock Report",
       },
       {
-        path: '/app/SupplierReport',
-        name: 'Supplier Report',
+        path: "/app/SupplierReport",
+        name: "Supplier Report",
       },
       {
-        path: '/app/QtyAlert',
-        name: 'Product Quantity Alert',
+        path: "/app/QtyAlert",
+        name: "Product Quantity Alert",
       },
       {
-        path: '/app/CustomerReport',
-        name: 'Customer Report',
+        path: "/app/CustomerReport",
+        name: "Customer Report",
       },
       {
-        path: '/app/BestCustomer',
-        name: 'Best Customer',
+        path: "/app/BestCustomer",
+        name: "Best Customer",
       },
       {
-        path: '/app/TopSell',
-        name: 'Top-Selling Product',
+        path: "/app/TopSell",
+        name: "Top-Selling Product",
       },
-    ]
+    ],
   },
+];
 
-  
-]
-
-export default routes
-
-
+export default routes;
