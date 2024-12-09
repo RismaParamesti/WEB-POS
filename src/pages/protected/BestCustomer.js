2ca9680e93a119315196cce34c35cfe264e12a56
Reihan Rachma Shafira <163364264+rrsfira@users.noreply.github.com>
2024-12-09 21:42:43 +0700

    Update BestCustomer.js

--- src/pages/protected/BestCustomer.js
+++ src/pages/protected/BestCustomer.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { setPageTitle } from '../../features/common/headerSlice'
-import BestCustomer from '../../features/report/bestcusotmer'
+import BestCustomer from '../../features/report/bestcustomer'
 
 function InternalPage(){
     const dispatch = useDispatch()
